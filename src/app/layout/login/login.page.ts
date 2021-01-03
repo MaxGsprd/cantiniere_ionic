bef8a93d9473e85d58200670fdd5d4210a2a6743
MaxGsprd <max.gaspard@outlook.fr>
2021-01-03 10:58:27 +0100

    add helper folder and auth system

--- src/app/layout/login/login.page.ts
+++ src/app/layout/login/login.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder,Validators } from '@angular/forms';
-// import { AuthService } from 'src/service/auth.service';
-// import { TokenStorageService } from 'src/service/token-storage.service';
-// import jwt_decode from 'jwt-decode';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-login',
@@ -15,46 +15,48 @@ export class LoginPage implements OnInit {
   isLoginFailed = false;
   errorMessage = '';
 
-  constructor(private fb: FormBuilder) { }
-  // constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private fb:FormBuilder) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+    this.editForm();
+  }
+  
+  editForm(){
     this.userProfile = this.fb.group({
       email:['',Validators.required],
       password:['',Validators.required],
     })
   }
-  
-  editForm(){
-  }
 
   onSubmit(): void {
     console.log(this.userProfile.value);
-    // const form = this.userProfile.value;
-    // this.authService.login(JSON.stringify(form))
-    // .then( res => {
-    //     console.log(res);
-    //     let token = res.headers.get('Authorization').split('Bearer').pop()
-    //     let user = jwt_decode(token);
-    //     this.tokenStorage.saveToken(token);
-    //     this.tokenStorage.saveUser(user);
-
-    //     this.isLoginFailed = false;
-    //     this.isLoggedIn = true;
-    //     this.reloadPage();
+    const form = this.userProfile.value;
+    this.authService.login(JSON.stringify(form))
+    .then( res => {
+        console.log(res);
+        let token = res.headers.get('Authorization').split('Bearer').pop()
+        let user = jwt_decode(token);
+        this.tokenStorage.saveToken(token);
+        this.tokenStorage.saveUser(user);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.reloadPage();
+    },
+    ).catch(err => {
+      console.log('err : ', err);
+      this.errorMessage = err.error.message;
+      //console.log(this.errorMessage);
+      this.isLoginFailed = true;
     }
-    // ).catch(err => {
-    //   console.log('err : ', err);
-    //   this.errorMessage = err.error.message;
-    //   //console.log(this.errorMessage);
-    //   // this.isLoginFailed = true;
-    // }
+    )
+  }
 
-  // }
-
-  // reloadPage(): void {
-  //   window.location.reload();
-  // }
+  reloadPage(): void {
+    window.location.reload();
+  }
 
 
 
