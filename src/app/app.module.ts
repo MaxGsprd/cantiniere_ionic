6e474e1271ae34082ff1557a6e4607377392b6a6
MaxGsprd <max.gaspard@outlook.fr>
2021-01-03 10:32:46 +0100

    add auth service and httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -18,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot(),
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     StatusBar,
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  api_url = "http://localhost:8080/lunchtime/";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http : HttpClient) { }
+
+  login(credentials) : Promise<any> {
+    return this.http.post<any>(this.api_url+'login', credentials, {observe: "response"}).toPromise();
+  }
+  
+  forgotPassword(email): Observable<any>  {
+    let obs: Observable<any>
+    const url = this.api_url+'forgotpassword';
+    obs = this.http.post(url+"?email=" + email.email,{})
+    console.log(obs)
+    return obs;
+  };
+}
