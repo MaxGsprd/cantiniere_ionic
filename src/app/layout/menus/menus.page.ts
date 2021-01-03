386de9fba84d351a652271b7e78b6cd63487f7e5
MaxGsprd <max.gaspard@outlook.fr>
2021-01-03 15:47:52 +0100

    add cantiniere service and menu layout

--- src/app/layout/menus/menus.page.ts
+++ src/app/layout/menus/menus.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CantiniereService } from "src/app/services/cantiniere.service";
 
 @Component({
   selector: 'app-menus',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenusPage implements OnInit {
 
-  constructor() { }
+  listeMenus = [];
+  panelOpenState = false;
+  radioSelected: any;
+  allMenus = [];
+
+  constructor(private cantiniere_api : CantiniereService) { }
 
   ngOnInit() {
+    this.getWeekMeals();
   }
+
+  async getWeekMenus(){
+    const response = await this.cantiniere_api.getWeekMenus();
+    this.listeMenus = response;
+    console.log(this.listeMenus);
+    this.listeMenus.forEach(element => {
+      this.getImageMenu(element.id);
+      if(element.meals) {
+        element.meals.forEach(element_meal => {
+          this.getImageMeal(element_meal.id);
+        });
+      }
+    });
+  }
+
+  async getWeekMeals() {
+    const response = await this.cantiniere_api.getWeekMeal();
+    this.listeMenus = response;
+    console.log(this.listeMenus)
+    this.listeMenus.forEach(element => {
+      this.getImageMealbyId(element.id);
+    })
+  }
+
+  async getImageMenu(id_menu) {
+    const response = await this.cantiniere_api.getImageMenus(id_menu);
+    this.listeMenus.forEach(element => {
+      if(element.imageId === response.id){
+        element.img = response.image64;
+      }
+    });
+    
+  }
+
+  async getImageMeal(id_meal: number) {
+    const response = await this.cantiniere_api.getImageMeal(id_meal);
+    this.listeMenus.forEach(element => {
+      if(element.meals) {
+        element.meals.forEach(element_meal => {
+          if(element_meal.imageId === response.id){
+            element_meal.img64 = response.image64;
+          }
+        });
+      }
+    })
+  }
+
+  async getImageMealbyId(id_meal: number) {
+    const response = await this.cantiniere_api.getImageMeal(id_meal);
+    this.listeMenus.forEach(element => {
+          if(element.imageId === response.id){
+            element.img64 = response.image64;
+          }
+        })
+  }
+
+
+  addPanier(id_menu: number) {
+    var id_meal = this.radioSelected;
+    var quantity: number = +(<HTMLInputElement>document.getElementById(this.radioSelected.toString())).value;
+    var order = {
+      quantity: quantity,
+      mealId: id_meal,
+      menuId: id_menu
+    }
+    console.log(order);
+
+}
 
 }
