7dcc0823bdbd1ad8885302b297487e19f2134232
relesseAston <relesse.medahi@social.aston-ecole.com>
2021-01-10 17:02:33 +0100

    menu bar naw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,35 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MenuController } from '@ionic/angular';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { CartPage } from './layout/cart/cart.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  providers: [CartPage]
 })
 export class AppComponent {
+  currentUser: any;
+  isLogged :boolean = false;
+  isLunchLady: Boolean;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController,
+    private token_service: TokenStorageService,
+    public cart: CartPage
   ) {
     this.initializeApp();
+    if(this.token_service.getUser()) {
+      this.currentUser = this.token_service.getUser().user;
+      this.isLunchLady = this.currentUser.isLunchLady;
+      this.isLogged = true;
+    }
   }
 
   initializeApp() {
@@ -24,4 +40,15 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  logout() {
+    this.token_service.signOut();
+    window.location.reload();
+  }
+
 }
